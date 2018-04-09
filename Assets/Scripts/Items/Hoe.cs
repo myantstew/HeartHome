34b261fc298ae16ab922ba7e35b1fc231b283174
saporter_mac <scott.porter.ii@gmail.com>
2018-04-09 18:56:47 -0500

    Added Soil

---
+++ Assets/Scripts/Items/Hoe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoe : Item {
+    public GameObject FertileSoilPrefab;
+
+    PlayerAction playerAction;
+    CharacterMovement playerMovement;
+
+    private void Awake()
+    {
+        var player = GameObject.Find("Player");
+        playerAction = player.GetComponent<PlayerAction>();
+        playerMovement = player.GetComponent<CharacterMovement>();
+
+        if(playerAction == null || playerMovement == null)
+        {
+            Debug.LogError("PlayerAction or CharacterMovement not found in scene.  Hoe needs both to function properly");
+        }
+
+    }
+
+    public override void UseAt(GameObject location)
+    {
+        if(playerAction.DetectedItem == null)
+        {
+            var soil = Instantiate(FertileSoilPrefab);
+            //soil.transform.position = playerMovemen
+        }
+    }
+	
+}
