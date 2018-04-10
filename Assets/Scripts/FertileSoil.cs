ad05e7a6175aeb304ce3b2d63bd1e0e947e6e170
saporter_mac <scott.porter.ii@gmail.com>
2018-04-09 19:51:17 -0500

    FertileSoil behavior and Seed Use behavior added

---
+++ Assets/Scripts/FertileSoil.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FertileSoil : MonoBehaviour {
+    public GameObject ThrownSeedsPrefab;
+
+    private bool seedsPlanted = false;
+    public bool SeedsPlanted { get { return seedsPlanted; } }
+	
+    public void PlantSeeds()
+    {
+        seedsPlanted = true;
+        Instantiate(ThrownSeedsPrefab, transform.position, Quaternion.Euler(0f, 0f, 0f), transform);
+    }
+}
--- Assets/Scripts/Items/Seeds.cs
+++ Assets/Scripts/Items/Seeds.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class Seeds : Item {
 
-	
+    PlayerAction playerAction;
+    CharacterMovement playerMovement;
+
+    private void Awake()
+    {
+        var player = GameObject.Find("Player");
+        playerAction = player.GetComponent<PlayerAction>();
+        playerMovement = player.GetComponent<CharacterMovement>();
+
+        if (playerAction == null || playerMovement == null)
+        {
+            Debug.LogError("PlayerAction or CharacterMovement not found in scene.  Hoe needs both to function properly");
+        }
+
+    }
+
+    public override void UseAt(GameObject location)
+    {
+        FertileSoil soil = location.GetComponent<FertileSoil>();
+        if (soil != null)
+        {
+            soil.PlantSeeds();
+        }
+    }
 }
